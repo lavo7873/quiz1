8d148644b95a0706d025bb0f574ef45272137ea4
lavo7873 <linhuang1101@gmail.com>
2023-02-09 18:17:34 -0800

    update

---
+++ main.js
@@ -0,0 +1,111 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+const BOT_IMG = "bot.png";
+const PERSON_IMG = "user.png";
+const BOT_NAME = "BOT";
+const PERSON_NAME = "Bạn";
+const prompts = [
+  ["co kieu anh beo"],
+  ["hi", "hey", "hello","xin chao"],
+  ["chao buoi sang"],
+  ["ban ten la gi"],
+  ["hom nay thoi tiet nhu nao"],
+  ["bay gio la may gio"],
+  ["ban an com chua"],
+  ["yeu anh khong"]
+]
+  
+const replies = [
+  ["Cô Kiều Anh vô cùng béo"],
+  ["Xin chào bạn"],
+  ["xin chào bạn, chúc bạn buổi sáng tốt lành"],
+  ["Tớ tên là BOT của Công"],
+  ["Hôm nay là một ngày trời nắng to, nên bạn ra đường mang áo dài tay nhé!"],
+  ["Tôi không biết"],
+  ["Tôi ăn rồi, còn bạn?"],
+  ["yêu cái địt mẹ này"]
+
+];
+const alternative = [
+ 
+  "Tôi không hiểu bạn đang nói gì"
+]
+const robot = ["How do you do, fellow human", "I am not a bot"];
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+  msgerInput.value = "";
+  addChat(PERSON_NAME, PERSON_IMG, "right", msgText);
+  output(msgText);
+});
+function output(input) {
+  let product;
+  let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
+  text = text
+    .replace(/ a /g, " ")  
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "")
+    .replace(/r u/g, "are you");
+  if (compare(prompts, replies, text)) {
+    product = compare(prompts, replies, text);
+  } else if (text.match(/thank/gi)) {
+    product = "You're welcome!"
+  } else if (text.match(/(robot|bot|robo)/gi)) {
+    product = robot[Math.floor(Math.random() * robot.length)];
+  } else {
+    product = alternative[Math.floor(Math.random() * alternative.length)];
+  }
+  const delay = input.split(" ").length * 100;
+  setTimeout(() => {
+    addChat(BOT_NAME, BOT_IMG, "left", product);
+  }, delay);
+}
+function compare(promptsArray, repliesArray, string) {
+  let reply;
+  let replyFound = false;
+  for (let x = 0; x < promptsArray.length; x++) {
+    for (let y = 0; y < promptsArray[x].length; y++) {
+      if (promptsArray[x][y] === string) {
+        let replies = repliesArray[x];
+        reply = replies[Math.floor(Math.random() * replies.length)];
+        replyFound = true;
+        break;
+      }
+    }
+    if (replyFound) {
+      break;
+    }
+  }
+  return reply;
+}
+function addChat(name, img, side, text) {
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
